d77dfe2eb44b3436a00b3b7f0212f63ae3f5c9df
Antoine Beyeler <abeyeler@ab-ware.com>
2019-06-20 16:09:25 +0200

    Improved admin interface
    - Player and PlayerUnit removed
    - Added read-only game data (Unit, Skill, Gear

--- sqds/admin.py
+++ sqds/admin.py
@@ -1,76 +1,40 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from django.db.models.functions import Lower
-
 from django_admin_listfilter_dropdown.filters import DropdownFilter
 
-from .models import Unit, Player, PlayerUnit, ScoredUnit, \
-    ScoreZetaRule, ScoreStatRule, Skill
+from .models import Unit, ScoredUnit, ScoreZetaRule, ScoreStatRule, Skill, Gear
+
+
+# noinspection PyMethodMayBeStatic,PyUnusedLocal
+class ReadOnlyMixin:
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class SkillInline(ReadOnlyMixin, admin.TabularInline):
+    model = Skill
+    extra = 0
 
 
 @admin.register(Unit)
-class UnitAdmin(admin.ModelAdmin):
+class UnitAdmin(ReadOnlyMixin, admin.ModelAdmin):
+    inlines = [SkillInline]
+
+
+@admin.register(Gear)
+class GearAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
 
 
-@admin.register(Player)
-class PlayerAdmin(admin.ModelAdmin):
+@admin.register(Skill)
+class SkillAdmin(ReadOnlyMixin, admin.ModelAdmin):
     pass
-
-
-def player_unit_summary(obj):
-    return format_html("{} <b>{}</b> {}",
-                       obj.star_count(),
-                       obj.level,
-                       obj.colored_gear())
-
-
-player_unit_summary.short_description = 'Level'
-
-
-class PlayerFilter(admin.SimpleListFilter):
-    title = 'Player'
-    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
-    parameter_name = 'player'
-
-    def lookups(self, request, model_admin):
-        qs = model_admin.get_queryset(request)
-        return [(i, i) for i in qs.values_list('player__name', flat=True)
-            .distinct().order_by(Lower('player__name'))]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(player__name__exact=self.value())
-
-
-class UnitFilter(admin.SimpleListFilter):
-    title = 'Unit'
-    template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
-    parameter_name = 'unit'
-
-    def lookups(self, request, model_admin):
-        qs = model_admin.get_queryset(request)
-        return [(i, i) for i in qs.values_list('unit__name', flat=True)
-            .distinct().order_by(Lower('unit__name'))]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(unit__name__exact=self.value())
-
-
-@admin.register(PlayerUnit)
-class PlayerUnitAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'gp', player_unit_summary,
-                    'speed', 'health', 'protection')
-
-    list_filter = (
-        PlayerFilter,
-        UnitFilter,
-        ('rarity', DropdownFilter),
-        ('level', DropdownFilter))
-
-    def has_view_permission(self, request, obj=None):
-        return False
 
 
 class ScoreStatRuleInline(admin.TabularInline):
@@ -120,4 +84,3 @@ class ScoredUnitAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
-
