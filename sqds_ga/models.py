8638cc076facf963667b68ec1109a196fd13e490
Antoine Beyeler <abeyeler@ab-ware.com>
2019-08-04 15:37:18 +0200

    Added GA pools to admin interface (read-only)

--- sqds_ga/models.py
+++ sqds_ga/models.py
@@ -9,9 +9,19 @@ class GAPool(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):  # pragma: no cover
+        return f"GAPool({self.focus_player.__str__()})"
+
+    class Meta:
+        verbose_name = 'GA pool'
+
 
 class GAPoolPlayer(models.Model):
     ga_pool = models.ForeignKey(GAPool, on_delete=models.CASCADE,
                                 related_name='ga_pool_player_set')
     player = models.ForeignKey(Player, on_delete=models.CASCADE,
                                related_name='ga_pool_player_set')
+
+    def __str__(self):  # pragma: no cover
+        return (f"GAPoolPlayer({self.player.__str__()} -> "
+                f"{self.ga_pool.focus_player.__str__()})")
