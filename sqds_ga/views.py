aba4f4da1a34689b77e54796748a9c91826d9bd5
Antoine Beyeler <abeyeler@ab-ware.com>
2019-08-14 17:16:45 +0200

    Added meta to GA Pool page (closes #20)

--- sqds_ga/views.py
+++ sqds_ga/views.py
@@ -1,5 +1,5 @@
 from django.db import transaction
-from django.http import HttpResponseNotFound, HttpResponse
+from django.http import HttpResponseNotFound
 from django.shortcuts import get_object_or_404, redirect
 from django.views.decorators.http import require_http_methods
 from django.views.generic import DetailView
@@ -40,24 +40,36 @@ class GAPoolView(MetadataMixin, DetailView):
     model = GAPool
     template_name = 'sqds_ga/ga_pool_overview.html'
 
+    # noinspection PyAttributeOutsideInit
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+
+        # We load other object as well
+        self.focus_player = (Player.objects
+                             .filter(pk=obj.focus_player.pk)
+                             .annotate_stats()
+                             .annotate_faction_gp()
+                             .select_related('guild')
+                             .first())
+        self.players = list(Player.objects
+                            .filter(ga_pool_player_set__ga_pool=obj)
+                            .select_related('guild')
+                            .annotate_faction_gp()
+                            .annotate_stats())
+        return obj
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['focus_player'] = (Player.objects
-                                   .filter(pk=self.object.focus_player.pk)
-                                   .annotate_stats()
-                                   .annotate_faction_gp()
-                                   .select_related('guild')
-                                   .first())
-        context['players'] = list(Player.objects
-                                  .filter(ga_pool_player_set__ga_pool=self.object)
-                                  .select_related('guild')
-                                  .annotate_faction_gp()
-                                  .annotate_stats())
+        context['focus_player'] = self.focus_player
+        context['players'] = self.players
         context['all_players'] = [context['focus_player'], *context['players']]
         return context
 
-    def get_meta_title(self, **kwargs):
-        pass
+    def get_meta_title(self, context=None):
+        return f"GA Pool ({self.focus_player.name})"
 
     def get_meta_description(self, context=None):
-        pass
+        return "{} vs. {} ({})".format(
+            self.focus_player.name,
+            ', '.join([p.name for p in self.players]),
+            self.object.created)
